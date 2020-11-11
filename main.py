a5c7b5806e63877ace81c5202ccaf1725b0a351d
Shreyansh-sinha <shreyanshsinha2@gmail.com>
2020-11-11 12:10:13 +0530

    Learnt to use Database in FLask by building a webapp

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from flask import Flask, render_template
+'''
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/CodingThunder'
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+'''
+'''
+		SNo, Name, Email, Phone_Num, Message, Date
+'''
+'''
+	SNo = db.Column(db.Integer, primary_key=True)
+	Name = db.Column(db.String(80), nullable=False)
+	Email = db.Column(db.String(20), nullable=False)
+	Phone_Num = db.Column(db.String(10), nullable=False)
+	Message = db.Column(db.String(120), nullable=False)
+	Date = db.Column(db.String(12), nullable=True)
 
 @app.route('/')
 def home():
@@ -9,8 +29,18 @@ def home():
 def about():
 	return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+	if(request.method == "POST"):
+		name = request.form.get('name')
+		email = request.form.get('email')
+		phno = request.form.get('phno')
+		message = request.form.get('msg')
+		
+		entry = Contacts(Name=name, Email=email, Phone_Num=phno, Message=message, Date=datetime.now())
+		db.session.add(entry)
+		db.session.commit()
+		
 	return render_template('contact.html')
 
 @app.route('/post')
@@ -18,3 +48,54 @@ def services():
 	return render_template('post.html')
 
 app.run(debug=True)
+'''
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/CodingThunder'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False;
+
+db = SQLAlchemy(app)
+
+
+class contacts(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    __tablename__ = 'contacts'
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = contacts(name=name, phone_num = phone, msg = message,email = email )
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
+
+
+app.run(debug=True)
+
+
